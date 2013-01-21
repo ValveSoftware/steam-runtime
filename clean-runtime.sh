9e69ebc58e74b40964be2954dc5b54098a2b8758
Sam Lantinga <slouken@libsdl.org>
2013-01-20 20:22:59 -0800

    Cleaning the runtime packages is fast, just use a one line status

--- clean-runtime.sh
+++ clean-runtime.sh
@@ -7,12 +7,10 @@ ARCHITECTURES="i386 amd64"
 TOP=$(cd "${0%/*}" && echo ${PWD})
 cd "${TOP}"
 
-echo "Removing all packages for ${ARCHITECTURES} ..."
+echo "Removing runtime packages for ${ARCHITECTURES}"
 
 for ARCHITECTURE in ${ARCHITECTURES}; do
     rm -rf "${TOP}/runtime/${ARCHITECTURE}"
 done
-
-echo "Done!"
 
 # vi: ts=4 sw=4 expandtab
