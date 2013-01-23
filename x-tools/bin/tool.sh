03da0a2aa7b302205a69bde973ffa4bf55e41b91
Sam Lantinga <slouken@libsdl.org>
2013-01-23 15:24:09 -0800

    Try to infer the build and runtime configuration if environment variables aren't set. Add include and library paths to the end of the compiler command line so they don't override normal command line parameters.

--- x-tools/bin/tool.sh
+++ x-tools/bin/tool.sh
@@ -7,9 +7,37 @@ PROGRAM=$(basename "$0")
 # The top level of the cross-compiler tree
 TOP=$(cd "${0%/*}" && echo "${PWD}/..")
 
-if [ -z "${HOST_ARCH}" -o -z "${TARGET_ARCH}" -o -z "${STEAM_RUNTIME}" ]; then
-    echo "Missing development environment variables, did you run shell.sh?"
-    exit 1
+if [ -z "${HOST_ARCH}" ]; then
+    export HOST_ARCH=$(dpkg --print-architecture)
+fi
+
+if [ -z "${TARGET_ARCH}" ]; then
+    for arg in "$@"; do
+        if [ "${arg}" = "-m32" ]; then
+            TARGET_ARCH="i386"
+            break
+        elif [ "${arg}" = "-m64" ]; then
+            TARGET_ARCH="amd64"
+            break
+        fi
+    done
+    if [ -z "${TARGET_ARCH}" ]; then
+        TARGET_ARCH="${HOST_ARCH}"
+    fi
+    export TARGET_ARCH
+fi
+            
+if [ -z "${STEAM_RUNTIME}" ]; then
+    if [ -d "${TOP}/runtime/${TARGET_ARCH}" ]; then
+        STEAM_RUNTIME="${TOP}/runtime/${TARGET_ARCH}"
+    elif [ -d "${TOP}/../runtime/${TARGET_ARCH}" ]; then
+        STEAM_RUNTIME="${TOP}/../runtime/${TARGET_ARCH}"
+    fi
+    if [ ! -d "${STEAM_RUNTIME}" ]; then
+        echo "Couldn't find runtime directory ${STEAM_RUNTIME}" >&2
+        exit 2
+    fi
+    export STEAM_RUNTIME
 fi
 
 case "${TARGET_ARCH}" in
@@ -28,20 +56,10 @@ amd64)
 esac
 CROSSTOOL_PATH="${TOP}/${HOST_ARCH}/${CROSSTOOL_PREFIX}/bin"
 
-# Function to insert an argument into a bash array
-function insert_arg()
+# Function to append an argument to the end of a bash array
+function append_arg()
 {
-    ARG=$1
-
-    # Shift other elements up
-    i=${#ARGS[@]}
-    while [ $i -gt 0 ]; do
-        ARGS[$i]="${ARGS[$(expr $i - 1)]}"
-        i=$(expr $i - 1)
-    done
-
-    # Add the new argument
-    ARGS[0]="${ARG}"
+    ARGS+=("$1")
 }
 
 # Add any additional command line parameters
@@ -49,11 +67,11 @@ declare -a ARGS
 ARGS=("$@")
 case "${PROGRAM}" in
     gcc|g++)
-        insert_arg "-I${STEAM_RUNTIME}/usr/include"
-        insert_arg "-L${STEAM_RUNTIME}/usr/lib"
-        insert_arg "-Wl,-rpath-link=${STEAM_RUNTIME}/usr/lib"
-        insert_arg "-L${STEAM_RUNTIME}/usr/lib/${CROSSTOOL_LIBPATH}"
-        insert_arg "-Wl,-rpath-link=${STEAM_RUNTIME}/usr/lib/${CROSSTOOL_LIBPATH}"
+        append_arg "-I${STEAM_RUNTIME}/usr/include"
+        append_arg "-L${STEAM_RUNTIME}/usr/lib"
+        append_arg "-Wl,-rpath-link=${STEAM_RUNTIME}/usr/lib"
+        append_arg "-L${STEAM_RUNTIME}/usr/lib/${CROSSTOOL_LIBPATH}"
+        append_arg "-Wl,-rpath-link=${STEAM_RUNTIME}/usr/lib/${CROSSTOOL_LIBPATH}"
         ;;
 esac
 
