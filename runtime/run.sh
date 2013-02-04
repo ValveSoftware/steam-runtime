d68fc7b5d4323fde1e6a9b202a67f6473339561a
Sam Lantinga <slouken@libsdl.org>
2013-02-04 02:49:08 -0800

    Set the STEAM_RUNTIME environment variable from run.sh

--- runtime/run.sh
+++ runtime/run.sh
@@ -13,6 +13,7 @@ fi
 # Note that we put the Steam runtime first
 # If ldd on a program shows any library in the system path, then that program
 # may not run in the Steam runtime.
+export STEAM_RUNTIME="${TOP}"
 export LD_LIBRARY_PATH="${TOP}/amd64/lib/x86_64-linux-gnu:${TOP}/amd64/lib:${TOP}/amd64/usr/lib/x86_64-linux-gnu:${TOP}/amd64/usr/lib:${TOP}/i386/lib/i386-linux-gnu:${TOP}/i386/lib:${TOP}/i386/usr/lib/i386-linux-gnu:${TOP}/i386/usr/lib:${LD_LIBRARY_PATH}"
 
 exec "$@"
