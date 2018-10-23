e8d03c0beba1fd8bb92d2387c5e8ca11a9bacd13
Simon McVittie <smcv@collabora.com>
2018-10-23 17:53:21 +0100

    tests: Rewrap long lines
    Now that this test is using 4-space indentation, it's unnecessary to
    wrap long lines quite so aggressively.
    
    Signed-off-by: Simon McVittie <smcv@collabora.com>

--- tests/all-packages-published.py
+++ tests/all-packages-published.py
@@ -33,10 +33,8 @@ if __name__ == '__main__':
 
                 if line <= last:
                     print(
-                        'warning: '
-                        'sourcepkgs.list:%d: '
-                        'not in `LC_ALL=C sort -u` '
-                        'order near %r'
+                        'warning: sourcepkgs.list:%d: not in '
+                        '`LC_ALL=C sort -u` order near %r'
                         % (i, line),
                         file=sys.stderr)
 
@@ -45,8 +43,8 @@ if __name__ == '__main__':
     if source_pkgs:
         for p in source_pkgs:
             print(
-                'error: source package %s is listed in '
-                'sourcepkgs.list but not in packages.txt'
+                'error: source package %s is listed in sourcepkgs.list '
+                'but not in packages.txt'
                 % p,
                 file=sys.stderr)
             fail = True
