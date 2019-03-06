632d8d24d11dff1e6b86b5a21db1198daff73f12
Simon McVittie <smcv@collabora.com>
2019-03-06 11:38:39 +0000

    setup_chroot.sh: Fix shellcheck warnings
    Signed-off-by: Simon McVittie <smcv@collabora.com>

--- setup_chroot.sh
+++ setup_chroot.sh
@@ -23,25 +23,32 @@ if [[ $(tput colors 2>/dev/null || echo 0) -gt 0 ]]; then
   COLOR_OFF=$'\e[0m'
 fi
 
-sh_quote() { local quoted="$(printf '%q ' "$@")"; [[ $# -eq 0 ]] || echo "${quoted:0:-1}"; }
+sh_quote ()
+{
+  local quoted
+  if [ $# -gt 0 ]; then
+    quoted="$(printf '%q ' "$@")"
+    echo "${quoted:0:-1}"
+  fi
+}
 
 prebuild_chroot()
 {
 	# install some packages
-	echo -e "\n${COLOR_ON}Installing debootstrap schroot...${COLOR_OFF}"
+	echo -e "\\n${COLOR_ON}Installing debootstrap schroot...${COLOR_OFF}"
 	sudo -E apt-get install -y debootstrap schroot
 
 	# Check if there are any active schroot sessions right now and warn if so...
 	schroot_list=$(schroot --list --all-sessions | head -n 1)
-	if [ $schroot_list ]; then
+	if [ -n "$schroot_list" ]; then
 		tput setaf 3
-		echo -e "\nWARNING: Schroot says you have a currently active session!\n"
+		echo -e "\\nWARNING: Schroot says you have a currently active session!\\n"
 		tput sgr0
 		echo "  ${schroot_list}"
 		echo ""
-		read -p "Are you sure you want to continue (y/n)? "
+		read -r -p "Are you sure you want to continue (y/n)? "
 		if [[ "$REPLY" != [Yy] ]]; then
-			echo -e "Cancelled...\n"
+			echo -e "Cancelled...\\n"
 			exit 1
 		fi
 	fi
@@ -58,9 +65,9 @@ prebuild_chroot()
 	done
 
 	if [[ "$STEAM_RUNTIME_SPEW_WARNING" == "1" ]]; then
-		read -p "  This ok (y/n)? "
+		read -r -p "  This ok (y/n)? "
 		if [[ "$REPLY" != [Yy] ]]; then
-			echo -e "Cancelled...\n"
+			echo -e "Cancelled...\\n"
 			exit 1
 		fi
 	fi
@@ -97,20 +104,20 @@ build_chroot()
 	CHROOT_NAME=${CHROOT_PREFIX}${pkg}
 
 	# blow away existing directories and recreate empty ones
-	echo -e "\n${COLOR_ON}Creating ${CHROOT_DIR}/${CHROOT_NAME}..."
+	echo -e "\\n${COLOR_ON}Creating ${CHROOT_DIR}/${CHROOT_NAME}..."
 	sudo rm -rf "${CHROOT_DIR}/${CHROOT_NAME}"
 	sudo mkdir -p "${CHROOT_DIR}/${CHROOT_NAME}"
 
 	# Create our schroot .conf file
-	echo -e "\n${COLOR_ON}Creating /etc/schroot/chroot.d/${CHROOT_NAME}.conf...${COLOR_OFF}" 
-	printf "[${CHROOT_NAME}]\ndescription=Ubuntu 12.04 Precise for ${pkg}\ndirectory=${CHROOT_DIR}/${CHROOT_NAME}\npersonality=${personality}\ngroups=sudo\nroot-groups=sudo\npreserve-environment=true\ntype=directory\n" | sudo tee /etc/schroot/chroot.d/${CHROOT_NAME}.conf
+	echo -e "\\n${COLOR_ON}Creating /etc/schroot/chroot.d/${CHROOT_NAME}.conf...${COLOR_OFF}"
+	printf '[%s]\ndescription=Ubuntu 12.04 Precise for %s\ndirectory=%s/%s\npersonality=%s\ngroups=sudo\nroot-groups=sudo\npreserve-environment=true\ntype=directory\n' "${CHROOT_NAME}" "${pkg}" "${CHROOT_DIR}" "${CHROOT_NAME}" "${personality}" | sudo tee "/etc/schroot/chroot.d/${CHROOT_NAME}.conf"
 
 	# Create our chroot
-	echo -e "\n${COLOR_ON}Bootstrap the chroot...${COLOR_OFF}" 
-	sudo -E debootstrap --arch=${pkg} --include=wget --keyring="${SCRIPT_DIR}/ubuntu-archive-keyring.gpg" precise ${CHROOT_DIR}/${CHROOT_NAME} http://archive.ubuntu.com/ubuntu/
+	echo -e "\\n${COLOR_ON}Bootstrap the chroot...${COLOR_OFF}"
+	sudo -E debootstrap --arch="${pkg}" --include=wget --keyring="${SCRIPT_DIR}/ubuntu-archive-keyring.gpg" precise "${CHROOT_DIR}/${CHROOT_NAME}" http://archive.ubuntu.com/ubuntu/
 
 	# Copy over proxy settings from host machine
-	echo -e "\n${COLOR_ON}Adding proxy info to chroot (if set)...${COLOR_OFF}"
+	echo -e "\\n${COLOR_ON}Adding proxy info to chroot (if set)...${COLOR_OFF}"
 	set +o pipefail
 	env | grep -i "_proxy=" | grep -v PERSISTENT_HISTORY_LAST | xargs -i echo export {} | sudo tee ${CHROOT_DIR}/${CHROOT_NAME}/etc/profile.d/steamrtproj.sh
 	env | grep -i "_proxy=" | grep -v PERSISTENT_HISTORY_LAST | xargs -i echo export {} | sudo tee -a ${CHROOT_DIR}/${CHROOT_NAME}/etc/environment
@@ -118,7 +125,7 @@ build_chroot()
 	sudo rm -rf "${CHROOT_DIR}/${CHROOT_NAME}/etc/apt/apt.conf"
 	if [ -f /etc/apt/apt.conf ]; then sudo cp "/etc/apt/apt.conf" "${CHROOT_DIR}/${CHROOT_NAME}/etc/apt"; fi  
 
-	echo -e "\n${COLOR_ON}Running ${BOOTSTRAP_SCRIPT}$(printf ' %q' "$@")...${COLOR_OFF}"
+	echo -e "\\n${COLOR_ON}Running ${BOOTSTRAP_SCRIPT}$(printf ' %q' "$@")...${COLOR_OFF}"
 
 	# Touch the logfile first so it has the proper permissions
 	rm -f "${LOGFILE}"
@@ -140,7 +147,7 @@ build_chroot()
 # http://stackoverflow.com/questions/64786/error-handling-in-bash
 function cleanup()
 {
-	echo -e "\nenv is:\n$(env)\n"
+	echo -e "\\nenv is:\\n$(env)\\n"
 	echo "ERROR: ${SCRIPTNAME} just hit error handler."
 	echo "  BASH_COMMAND is \"${BASH_COMMAND}\""
 	echo "  BASH_VERSION is $BASH_VERSION"
@@ -160,7 +167,7 @@ usage()
 	fi
 
 	echo "Usage: $0 [--beta | --suite SUITE] [--extra-apt-source 'deb http://MIRROR SUITE COMPONENT...'] [--output-dir <DIRNAME>] --i386 | --amd64"
-	exit $1
+	exit "$1"
 }
 
 main()
@@ -179,7 +186,7 @@ main()
 	while [ "$#" -gt 0 ]; do
 		case "$1" in
 			(--amd64|--i386)
-				arch_arguments+=($1)
+				arch_arguments+=("$1")
 				shift
 				;;
 
@@ -244,12 +251,12 @@ main()
 	done
 	trap - EXIT
 
-	echo -e "\n${COLOR_ON}Done...${COLOR_OFF}"
+	echo -e "\\n${COLOR_ON}Done...${COLOR_OFF}"
 }
 
 # Launch ourselves with script so we can time this and get a log file
 if [[ ! -v SETUP_CHROOT_LOGGING_STARTED ]]; then
-	if which script &>/dev/null; then
+	if command -v script >/dev/null; then
 		export SETUP_CHROOT_LOGGING_STARTED=1
 		export SHELL=/bin/bash
 		script --return --command "time $SCRIPT $(sh_quote "$@")" "${LOGFILE}"
