23249b1915fef4f98733fa152584c0aa9a89e7f6
Ludovico de Nittis <ludovico.denittis@collabora.com>
2020-04-23 14:03:57 +0200

    setup: show a proper progress bar with zenity
    Intead of `--pulsate < /dev/zero` that was consuming 100% of a single
    CPU core, now we provide a proper progress bar to inform users that
    something is in progress.
    
    With this new approach it will be also clear if the process gets stuck.
    
    (Fixes: T20668)
    
    Signed-off-by: Ludovico de Nittis <ludovico.denittis@collabora.com>

--- templates/setup.sh
+++ templates/setup.sh
@@ -17,24 +17,22 @@ fi
 
 pin_newer_runtime_libs ()
 {
-    # Is always set at this point, but may be empty if the host lacks zenity
-    if [ -n "${STEAM_ZENITY}" ]; then
-        "${STEAM_ZENITY}" --progress --pulsate --no-cancel --width 400 --text="Pins potentially out-of-date, rebuilding..." < /dev/zero &
-        zpid=$!
-    else
-        zpid=""
-    fi
-
     # Set separator to newline just for this function
     local IFS
     IFS=$(echo -en '\n\b')
 
     local steam_runtime_path
+    local zenity_progress=true
 
     # First argument is the runtime path
     steam_runtime_path=$(realpath "$1")
 
     if [[ ! -d "$steam_runtime_path" ]]; then return; fi
+
+    # Second optional argument is the zenity print progress flag
+    if [[ "$#" -gt 1 ]]; then
+        zenity_progress=$2
+    fi
 
     # Associative array; indices are the SONAME, values are final path
     local -A host_libraries_32
@@ -42,15 +40,20 @@ pin_newer_runtime_libs ()
 
     local bitness
     local final_library
+    local find_num
     local find_output
+    local find_output_array
     local h_lib_major
     local h_lib_minor
     local h_lib_third
     local host_library
     local host_soname_symlink
+    local ldconfig_num
     local ldconfig_output
+    local ldconfig_output_array
     local leftside
     local library_path_prefix
+    local n_done
     local r_lib_major
     local r_lib_minor
     local r_lib_third
@@ -62,9 +65,30 @@ pin_newer_runtime_libs ()
     rm -rf "$steam_runtime_path/pinned_libs_32"
     rm -rf "$steam_runtime_path/pinned_libs_64"
 
+    # By roughly timing the average execution time we divided the progress in three parts:
+    # The setup, 4% of the total
+    # The ldconfig, 60% of the total
+    # The pinning, 35% of the total
+    # And at the end we just print 100%
+    if [ "$zenity_progress" = true ]; then
+        echo 4
+    else
+        printf '\r 4%    \r'
+    fi
+
     # First, grab the list of system libraries from ldconfig and put them in the arrays
-    for ldconfig_output in $(/sbin/ldconfig -XNv 2> /dev/null)
+    mapfile -t ldconfig_output_array < <(/sbin/ldconfig -XNv 2> /dev/null)
+    ldconfig_num=${#ldconfig_output_array[@]}
+    n_done=0
+
+    for ldconfig_output in "${ldconfig_output_array[@]}"
     do
+        if [ "$zenity_progress" = true ]; then
+            echo $(( ( 60 * n_done ) / ldconfig_num + 4 ))
+        else
+            printf '\r %d%%    \r' $(( ( 60 * n_done ) / ldconfig_num + 4 ))
+        fi
+        (( n_done=n_done+1 ))
         # If line starts with a leading / and contains :, it's a new path prefix
         if [[ "$ldconfig_output" =~ ^/.*: ]]
         then
@@ -103,11 +127,27 @@ pin_newer_runtime_libs ()
         fi
     done
 
+    if [ "$zenity_progress" = true ]; then
+        echo 64
+    else
+        printf '\r 64%    \r'
+    fi
+
     mkdir "$steam_runtime_path/pinned_libs_32"
     mkdir "$steam_runtime_path/pinned_libs_64"
 
-    for find_output in $(find "$steam_runtime_path" -type l | grep \\\.so)
+    mapfile -t find_output_array < <(find "$steam_runtime_path" -type l | grep \\\.so)
+    find_num=${#find_output_array[@]}
+    n_done=0
+
+    for find_output in "${find_output_array[@]}"
     do
+        if [ "$zenity_progress" = true ]; then
+            echo $(( ( 35 * n_done ) / find_num + 64 ))
+        else
+            printf '\r %d%%    \r' $(( ( 35 * n_done ) / find_num + 64 ))
+        fi
+        (( n_done=n_done+1 ))
         # Left-hand side of soname symlink should be *.so.%d
         if [[ ! $find_output =~ .*\.so.[[:digit:]]+$ ]]; then continue; fi
 
@@ -220,19 +260,19 @@ pin_newer_runtime_libs ()
                 ;;
         esac
 
-
+        # Print to stderr because zenity is consuming stdout
         if [[ $runtime_version_newer == "yes" ]]; then
-            echo "Found newer runtime version for $bitness-bit $soname. Host: $h_lib_major.$h_lib_minor.$h_lib_third Runtime: $r_lib_major.$r_lib_minor.$r_lib_third"
+            >&2 echo "Found newer runtime version for $bitness-bit $soname. Host: $h_lib_major.$h_lib_minor.$h_lib_third Runtime: $r_lib_major.$r_lib_minor.$r_lib_third"
         elif [[ $runtime_version_newer == "forced" ]]; then
-            echo "Forced use of runtime version for $bitness-bit $soname. Host: $h_lib_major.$h_lib_minor.$h_lib_third Runtime: $r_lib_major.$r_lib_minor.$r_lib_third"
+            >&2 echo "Forced use of runtime version for $bitness-bit $soname. Host: $h_lib_major.$h_lib_minor.$h_lib_third Runtime: $r_lib_major.$r_lib_minor.$r_lib_third"
         fi
 
         if [[ $runtime_version_newer == "yes" \
               || $runtime_version_newer == "forced" ]]; then
             ln -s "$final_library" "$steam_runtime_path/pinned_libs_$bitness/$soname"
             # Keep track of the exact version name we saw on the system at pinning time to check later
-            echo "$host_soname_symlink" > "$steam_runtime_path/pinned_libs_$bitness/system_$soname"
-            echo "$host_library" >> "$steam_runtime_path/pinned_libs_$bitness/system_$soname"
+            >&2 echo "$host_soname_symlink" > "$steam_runtime_path/pinned_libs_$bitness/system_$soname"
+            >&2 echo "$host_library" >> "$steam_runtime_path/pinned_libs_$bitness/system_$soname"
             touch "$steam_runtime_path/pinned_libs_$bitness/has_pins"
         fi
     done
@@ -252,8 +292,10 @@ pin_newer_runtime_libs ()
         fi
     done
 
-    if [ -n "${zpid}" ]; then
-        kill $zpid
+    if [ "$zenity_progress" = true ]; then
+        echo 100
+    else
+        echo " 100%   "
     fi
 }
 
@@ -316,7 +358,12 @@ check_pins ()
     if [[ $pins_need_redoing == "yes" ]]
     then
         echo Pins potentially out-of-date, rebuilding...
-        pin_newer_runtime_libs "$steam_runtime_path"
+        # Is always set at this point, but may be empty if the host lacks zenity
+        if [ -n "${STEAM_ZENITY}" ]; then
+            pin_newer_runtime_libs "$steam_runtime_path" | "${STEAM_ZENITY}" --progress --auto-close --percentage=0 --no-cancel --width 400 --text="Pins potentially out-of-date, rebuilding..."
+        else
+            pin_newer_runtime_libs "$steam_runtime_path" "false"
+        fi
     else
         echo Pins up-to-date!
     fi
@@ -430,7 +477,12 @@ main ()
         (setup)
             if [ -n "$opt_force" ]
             then
-                pin_newer_runtime_libs "$top"
+                # Is always set at this point, but may be empty if the host lacks zenity
+                if [ -n "${STEAM_ZENITY}" ]; then
+                    pin_newer_runtime_libs "$top" | "${STEAM_ZENITY}" --progress --auto-close --percentage=0 --no-cancel --width 400 --text="Forcing rebuild of pins..."
+                else
+                    pin_newer_runtime_libs "$top" "false"
+                fi
             else
                 check_pins "$top"
             fi
