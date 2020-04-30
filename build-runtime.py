ValveSoftware/steam-runtime

commit 73a5d7b344d20d34c805e20bf5bb8979fc9c94a2

Use https for all URLs, except those that are secured by apt
As a general principle, we should have a trusted path to everything we
download, even if the network is malicious.

We don't need this for debos-chroot.yaml, scripts/bootstrap-runtime.sh,
setup_chroot.sh because they authenticate the packages they download
by using PGP keys (see apt-secure(8)). In older Debian derivatives
apt can't handle https on its own, and using https makes it harder to
go via a caching proxy, so it's undesirable to use https just on
principle.

Signed-off-by: Simon McVittie <smcv@collabora.com>

build-runtime.py
@@ -211,14 +211,14 @@ def parse_args():
 	parser.add_argument("--symbols", help="include debugging symbols", action="store_true")
 	parser.add_argument(
 		"--repo", help="main apt repository URL",
-		default="http://repo.steampowered.com/steamrt",
+		default="https://repo.steampowered.com/steamrt",
 	)
 	parser.add_argument(
 		"--extra-apt-source", dest='extra_apt_sources',
 		default=[], action='append',
 		help=(
 			"additional apt sources in the form "
-			"'deb http://URL SUITE COMPONENT [COMPONENT...]' "
+			"'deb https://URL SUITE COMPONENT [COMPONENT...]' "
 			"(may be repeated)"
 		),
 	)
@@ -1202,7 +1202,7 @@ for line in args.extra_apt_sources:
 	if len(tokens) < 4:
 		raise ValueError(
 			'--extra-apt-source argument must be in the form '
-			'"deb http://URL SUITE COMPONENT [COMPONENT...]"')
+			'"deb https://URL SUITE COMPONENT [COMPONENT...]"')
 
 	if tokens[0] not in ('deb', 'deb-src', 'both'):
 		raise ValueError(

scripts/bootstrap-runtime.sh
@@ -158,7 +158,7 @@ heredoc
   apt-get install --force-yes -y clang-3.6 lldb-3.6
 
   # Workaround bug 714890 in 32-bit clang. Gcc 4.8 changed the include paths.
-  # http://bugs.debian.org/cgi-bin/bugreport.cgi?bug=714890
+  # https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=714890
   #   /usr/include/c++/4.6/i686-linux-gnu/bits/c++config.h
   #   /usr/include/i386-linux-gnu/c++/4.8/bits/c++config.h
   if [ -d /usr/include/i386-linux-gnu/c++/4.8 ]; then

setup_chroot.sh
@@ -229,7 +229,7 @@ untar_chroot ()
 	fi
 }
 
-# http://stackoverflow.com/questions/64786/error-handling-in-bash
+# https://stackoverflow.com/questions/64786/error-handling-in-bash
 function cleanup()
 {
 	echo -e "\\nenv is:\\n$(env)\\n"

templates/COPYING
@@ -8,7 +8,7 @@ The scripts used to build these packages can be found on GitHub:
 https://github.com/ValveSoftware/steam-runtime
 
 The source code and debug versions of the packages can be found here:
-http://media.steampowered.com/client/runtime/PLACEHOLDER_URL
+https://repo.steampowered.com/PLACEHOLDER_URL
 
 
 The scripts and other documentation contributed by Valve are covered under

commit 4cdee931816e4e3ca9d4f52effbfaeb2858787dd

build-runtime: Substitute the new form of the placeholder URL
This regressed in 73a5d7b3.

Signed-off-by: Simon McVittie <smcv@collabora.com>

build-runtime.py
@@ -1295,7 +1295,7 @@ if args.debug_url:
 		) as writer:
 			for line in reader:
 				line = re.sub(
-					r'https?://media\.steampowered\.com/client/runtime/.*$',
+					r'https?://repo\.steampowered\.com/PLACEHOLDER.*$',
 					args.debug_url, line)
 				writer.write(line)
 